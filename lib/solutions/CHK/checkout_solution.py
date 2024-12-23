20b797bafb10f8f0e94cefd3e3f23c5c945f3e36
sbx_user1051 <sbx_user1051@169.254.151.250>
2024-12-23 10:23:35 +0000

    Mon Dec 23 10:23:35 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,32 +2,25 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+from typing import Counter
+
+
 def checkout(skus):
-    number_of_items = int(skus[0])
-    type_of_item = skus[1]
-    
-    if(type_of_item == 'C'):
-        return number_of_items * 20
-    
-    if(type_of_item == 'D'):
-        return number_of_items * 15
-    
-    if(type_of_item == 'A'):
-        remainder = number_of_items % 3
-        if(remainder == 0):
-            return (number_of_items/3)*130
-        else:
-            round_low = number_of_items - remainder
-            offer_return = round_low * 130 
-            return (remainder * 50) + offer_return
-        
-    if(type_of_item == 'B'):
-        remainder = number_of_items % 2
-        if(remainder == 0):
-            return (number_of_items/2)*45
-        else:
-            round_low = number_of_items - remainder
-            offer_return = round_low * 45 
-            return (remainder * 30) + offer_return
-    
+   items = ["A","B","C","D"]
+   if not all(char in items for char in skus):
     return -1
+
+   nr_of_items = Counter(skus)
+   total = 0
+   for item,count in nr_of_items.items():
+       if item == 'C':
+           total += count * 20
+       if item == 'D':
+           total += count * 15
+       if item == "A":
+          total += (count // 3) * 130
+          total += (count % 3) * 50
+       if item == "B":
+          total += (count // 2) * 45
+          total += (count % 2) * 30
+   return total
