5f40adb285edf57e8b2d3c11d7f635b09d088229
sbx_user1051 <sbx_user1051@169.254.151.250>
2024-12-23 11:05:07 +0000

    Mon Dec 23 11:05:07 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,14 +8,11 @@ from typing import Counter
 def checkout(skus):
    items_with_prices = {"A" : 50,"B":30,"C":20,"D":15,"E":40}
    offers= {"A":[(5,200),(3,130)],"B":[(2,45)]}
+   
    if not all(char in items_with_prices for char in skus):
     return -1
 
    nr_of_items = Counter(skus)
-   
-   if "E" in nr_of_items and "B" in nr_of_items:
-       free_b_item = nr_of_items["E"] // 2
-       nr_of_items["B"] = max(0,nr_of_items["B"]-free_b_item)
        
    total = 0
    for item,count in nr_of_items.items():
@@ -26,4 +23,10 @@ def checkout(skus):
            total += count * items_with_prices[item]
        else:
             total += count * items_with_prices[item] 
+            
+   if "E" in nr_of_items and "B" in nr_of_items:
+       free_b_item = nr_of_items["E"] // 2
+       total -= free_b_item * items_with_prices["B"]
+            
    return total
+
