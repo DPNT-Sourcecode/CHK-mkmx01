976446310a6552ec2671ab3a0a260bb01ddb579e
sbx_user1051 <sbx_user1051@169.254.151.250>
2024-12-23 12:47:06 +0000

    Mon Dec 23 12:47:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -26,13 +26,7 @@ def checkout(skus):
    nr_of_items_of_group_discount = sum(nr_of_items[product] for product in group_items)
    if nr_of_items_of_group_discount // 3 >= 1:
     total += (nr_of_items_of_group_discount // 3) * group_offer
-    rest_group_items = nr_of_items_of_group_discount % 3
-    print(nr_of_items_of_group_discount)
-    print(rest_group_items)
-    for item in sorted(group_items, key= lambda x:-items_with_prices[x]):
-        while rest_group_items >= 0 and nr_of_items[item] > 0:
-            nr_of_items[item] -= 1
-            rest_group_items -= 1
+    
            
            
    for item,count in nr_of_items.items():
@@ -62,6 +56,7 @@ def reduce_same_item(nr_of_items, main_item, initial_needed_ammount):
     return nr_of_items
 
 checkout("STX")
+
 
 
 
