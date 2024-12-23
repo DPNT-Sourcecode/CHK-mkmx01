4792e5e2cceaa9bc764cb986f608790a24a6ba20
sbx_user1051 <sbx_user1051@169.254.151.250>
2024-12-23 09:53:40 +0000

    Mon Dec 23 09:53:40 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,29 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    number_of_items = int(skus[0])
+    type_of_item = skus[1]
+    
+    if(type_of_item == 'C'):
+        return number_of_items * 20
+    
+    if(type_of_item == 'D'):
+        return number_of_items * 15
+    
+    if(type_of_item == 'A'):
+        remainder = number_of_items % 3
+        if(remainder == 0):
+            return (number_of_items/3)*130
+        else:
+            round_low = number_of_items - remainder
+            offer_return = round_low * 130 
+            return (remainder * 50) + offer_return
+        
+    if(type_of_item == 'B'):
+        remainder = number_of_items % 2
+        if(remainder == 0):
+            return (number_of_items/2)*45
+        else:
+            round_low = number_of_items - remainder
+            offer_return = round_low * 45 
+            return (remainder * 30) + offer_return
