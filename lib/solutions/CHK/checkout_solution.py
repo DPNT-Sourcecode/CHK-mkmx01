63ffcf8b13a027240a6e69afa75afee12f4598e2
sbx_user1051 <sbx_user1051@169.254.151.250>
2024-12-23 12:44:06 +0000

    Mon Dec 23 12:44:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -30,7 +30,7 @@ def checkout(skus):
     print(nr_of_items_of_group_discount)
     print(rest_group_items)
     for item in sorted(group_items, key= lambda x:-items_with_prices[x]):
-        while rest_group_items > 0 and nr_of_items[item] > 0:
+        while rest_group_items >= 0 and nr_of_items[item] > 0:
             nr_of_items[item] -= 1
             rest_group_items -= 1
            
@@ -62,6 +62,7 @@ def reduce_same_item(nr_of_items, main_item, initial_needed_ammount):
     return nr_of_items
 
 checkout("STX")
+
 
 
 
