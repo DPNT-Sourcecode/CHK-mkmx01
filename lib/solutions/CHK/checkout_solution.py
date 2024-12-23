3f252827ac74c6ff86daeb1b680a52d1d014deca
sbx_user1051 <sbx_user1051@169.254.151.250>
2024-12-23 11:20:35 +0000

    Mon Dec 23 11:20:35 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -23,9 +23,11 @@ def checkout(skus):
        if item in offers: 
            for count_number,count_price in sorted(offers[item],reverse=True):
                total += (count // count_number) * count_price
-               total += (count % count_number) * items_with_prices[item]
+               count % count_number
+           total += count * items_with_prices[item]  
        else:
             total += count * items_with_prices[item] 
         
             
    return total
+
